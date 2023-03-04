ebc0ecf1c8f771f0d762a5cb695290564f44d23a
mickybart <mickybart@pygoscelis.org>
2023-03-03 22:38:55 -0500

    unittests + integration tests

--- src/api/helloworld.rs
+++ src/api/helloworld.rs
@@ -1,6 +1,6 @@
 use actix_web::{get, web};
-use tracing::instrument;
 use serde::Deserialize;
+use tracing::instrument;
 
 pub fn register(cfg: &mut web::ServiceConfig) {
     cfg.service(helloworld).service(slow_world).service(hello);
@@ -18,7 +18,7 @@ async fn hello(name: web::Path<String>) -> String {
 
 #[derive(Deserialize)]
 struct SlowQuery {
-    times: Option<u8>
+    times: Option<u8>,
 }
 
 #[get("/slowworld")]
@@ -29,10 +29,96 @@ async fn slow_world(info: web::Query<SlowQuery>) -> &'static str {
         slow_down().await;
     }
 
-    "Hellooooo, Wooooorld"
+    "Hellooooo, Wooooorld !"
 }
 
 #[instrument]
 async fn slow_down() {
     tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{body::to_bytes, test, App};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_helloworld() {
+        let app = App::new().service(helloworld);
+        let app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        assert_eq!(bytes, b"Hello, World !"[..]);
+    }
+
+    #[actix_web::test]
+    async fn test_hello() {
+        let app = App::new().service(hello);
+        let app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri("/TEST").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        assert_eq!(bytes, b"Hello, TEST !"[..]);
+    }
+
+    #[actix_web::test]
+    async fn test_slowworld() {
+        let app = App::new().service(slow_world);
+        let app = test::init_service(app).await;
+
+        let req = test::TestRequest::get()
+            .uri("/slowworld?times=0")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        assert_eq!(bytes, b"Hellooooo, Wooooorld !"[..]);
+    }
+
+    #[actix_web::test]
+    async fn test_register() {
+        let app = App::new().configure(register);
+        let app = test::init_service(app).await;
+
+        // GET /slowworld
+        let req = test::TestRequest::get()
+            .uri("/slowworld?times=0")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        assert_eq!(bytes, b"Hellooooo, Wooooorld !"[..]);
+
+        // GET /TEST
+        let req = test::TestRequest::get().uri("/TEST").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        assert_eq!(bytes, b"Hello, TEST !"[..]);
+
+        // GET /
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        assert_eq!(bytes, b"Hello, World !"[..]);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,9 @@ async fn main() -> std::io::Result<()> {
     info!("listening on {}", addr);
 
     HttpServer::new(|| {
-        let app = App::new()
+        app!()
             .wrap(Logger::default().log_target("http_log").exclude("/health"))
             .wrap(RequestTracing::new())
-            .service(health)
-            .default_service(web::route().to(HttpResponse::MethodNotAllowed));
-
-        #[cfg(feature = "helloworld")]
-        let app = app.configure(crate::api::helloworld::register);
-
-        app
     })
     .bind(addr)?
     .run()
@@ -39,9 +32,123 @@ async fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+// WORKAROUND with macro as returning App from a function is not trivial (see multiple issues discussion on this topic)
+#[macro_export]
+macro_rules! app {
+    () => {{
+        let app = App::new()
+            .service(health)
+            .default_service(web::route().to(HttpResponse::MethodNotAllowed));
+
+        #[cfg(feature = "helloworld")]
+        let app = app.configure(crate::api::helloworld::register);
+
+        app
+    }};
+}
+
 #[get("/health")]
 async fn health() -> impl Responder {
     web::Json(json!({
         "status": "ok"
     }))
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{body::to_bytes, test, App};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_health() {
+        let app = App::new().service(health);
+        let app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri("/health").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        let json_body: serde_json::Value = serde_json::from_slice(&bytes[..]).unwrap();
+        assert_eq!(
+            json_body,
+            json!(
+                {
+                    "status": "ok"
+                }
+            )
+        );
+    }
+
+    #[actix_web::test]
+    async fn test_default() {
+        let app = test::init_service(app!()).await;
+
+        let req = test::TestRequest::get().uri("/something/else").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_client_error());
+    }
+
+    #[actix_web::test]
+    async fn test_microservice() {
+        let app = test::init_service(app!()).await;
+
+        // default route
+        let req = test::TestRequest::get().uri("/something/else").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_client_error());
+
+        // GET /health
+        let req = test::TestRequest::get().uri("/health").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let bytes = to_bytes(resp.into_body()).await.unwrap();
+        let json_body: serde_json::Value = serde_json::from_slice(&bytes[..]).unwrap();
+        assert_eq!(
+            json_body,
+            json!(
+                {
+                    "status": "ok"
+                }
+            )
+        );
+
+        #[cfg(feature = "helloworld")]
+        {
+            // GET /slowworld
+            let req = test::TestRequest::get()
+                .uri("/slowworld?times=0")
+                .to_request();
+            let resp = test::call_service(&app, req).await;
+
+            assert!(resp.status().is_success());
+
+            let bytes = to_bytes(resp.into_body()).await.unwrap();
+            assert_eq!(bytes, b"Hellooooo, Wooooorld !"[..]);
+
+            // GET /TEST
+            let req = test::TestRequest::get().uri("/TEST").to_request();
+            let resp = test::call_service(&app, req).await;
+
+            assert!(resp.status().is_success());
+
+            let bytes = to_bytes(resp.into_body()).await.unwrap();
+            assert_eq!(bytes, b"Hello, TEST !"[..]);
+
+            // GET /
+            let req = test::TestRequest::get().uri("/").to_request();
+            let resp = test::call_service(&app, req).await;
+
+            assert!(resp.status().is_success());
+
+            let bytes = to_bytes(resp.into_body()).await.unwrap();
+            assert_eq!(bytes, b"Hello, World !"[..]);
+        }
+    }
 }
